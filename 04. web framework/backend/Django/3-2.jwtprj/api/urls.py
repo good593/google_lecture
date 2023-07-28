0b5ff9007193dff3de8316f06989bb85504a32e7
good593 <goodwon593@gmail.com>
2023-07-28 21:09:17 +0900

    폴더 변경

--- 04. web framework/backend/Django/3-2.jwtprj/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path 
-from rest_framework_simplejwt import views as jwt_views 
-from .views import HelloWorld, Extractor
-
-urlpatterns = [
-    path('login/', jwt_views.TokenObtainPairView.as_view(), name='login'),
-    path('run/', HelloWorld.as_view(), name='hello-world'),
-    path('me/', Extractor.as_view(), name='extract-token')
-]
