0d58022e0fd6c140cd4dc68af7c2fae359472bc6
good593 <goodwon593@gmail.com>
2023-09-11 12:27:45 +0900

    react-basic

---
+++ 04. web framework/frontend/React/react-basic/src/App.js
@@ -0,0 +1,19 @@
+// import './App.css';
+
+import Home from "./pages/Home";
+// import About from "./pages/About";
+// import Counter from "./pages/Counter";
+// import Input from "./pages/Input";
+// import Login from "./pages/Login";
+// import UserList from "./pages/UserList";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
