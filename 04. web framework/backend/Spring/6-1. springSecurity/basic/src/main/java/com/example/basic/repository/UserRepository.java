a68a02a28faa8971984c1d93d226c2d606349102
good593 <goodwon593@gmail.com>
2023-09-17 14:00:45 +0900

    필요없는 파일 삭제

--- 04. web framework/backend/Spring/6-1. springSecurity/basic/src/main/java/com/example/basic/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.basic.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.basic.model.User;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-  
-}
